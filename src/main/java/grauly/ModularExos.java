46969e40bd2b535aeda6eafdac8483fc85f8d952
Nils Dellemann <nils.dellemann@web.de>
2022-09-24 14:29:50 +0200

    chore:  adapted template

---
+++ src/main/java/grauly/ModularExos.java
@@ -0,0 +1,15 @@
+package grauly;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModularExos implements ModInitializer {
+	public static final String MODID = "modularexos";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/grauly/ModularExosClient.java
@@ -0,0 +1,11 @@
+package grauly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModularExosClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
