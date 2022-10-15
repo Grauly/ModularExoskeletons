c96f1cff465a9123422a0d1037c524cd1a1ea887
Nils Dellemann <nils.dellemann@web.de>
2022-10-15 02:12:01 +0200

    feat: sprint speed increase on base exo

--- src/main/java/grauly/item/ExoItem.java
+++ src/main/java/grauly/item/ExoItem.java
@@ -1,8 +1,15 @@
 package grauly.item;
 
+import com.google.common.collect.Multimap;
+import dev.emi.trinkets.api.SlotAttributes;
+import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketItem;
-import grauly.modules.exo.ExoModule;
 import grauly.modules.base.ModularItem;
+import grauly.modules.exo.ExoModule;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -10,9 +17,13 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class ExoItem extends TrinketItem implements ModularItem<ExoModule> {
 
+    protected static final UUID SPRINT_SPEED_MODIFIER_UUID = UUID.fromString("b0373195-b96a-4271-852e-d5f05d46f345");
+    protected static final EntityAttributeModifier SPRINT_SPEED_MODIFIER = new EntityAttributeModifier(SPRINT_SPEED_MODIFIER_UUID, "modularexos:sprintspeedbonus", 0.03f, EntityAttributeModifier.Operation.ADDITION);
+    protected static UUID TEST_UUID;
     protected final ArrayList<ExoModule> allowedModuleList = new ArrayList<>();
 
     public ExoItem(Settings settings) {
@@ -21,17 +32,6 @@ public class ExoItem extends TrinketItem implements ModularItem<ExoModule> {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        var stack = user.getStackInHand(hand);
-        if(user.isSneaking()) {
-            var newModule = AllItems.EX_MODULE.getDefaultStack();
-            var list = this.deserializeModules(stack);
-            list.add(newModule);
-            this.serializeModules(list,stack);
-        } else {
-            this.getInstalledModules(stack).forEach(m -> {
-                System.out.println(m.getDescription());
-            });
-        }
         return super.use(world, user, hand);
     }
 
@@ -48,5 +48,40 @@ public class ExoItem extends TrinketItem implements ModularItem<ExoModule> {
     @Override
     public void recalculateStats(ItemStack stack) {
 
+    }
+
+    @Override
+    public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        super.onUnequip(stack, slot, entity);
+        var attributeInstance = entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (attributeInstance != null) {
+            if (attributeInstance.hasModifier(SPRINT_SPEED_MODIFIER)) {
+                attributeInstance.removeModifier(SPRINT_SPEED_MODIFIER);
+            }
+        }
+    }
+
+    @Override
+    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
+        var attributeInstance = entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (attributeInstance != null)
+            if (entity.isSprinting()) {
+                if (!attributeInstance.hasModifier(SPRINT_SPEED_MODIFIER)) {
+                    attributeInstance.addTemporaryModifier(SPRINT_SPEED_MODIFIER);
+                }
+            } else {
+                if (attributeInstance.hasModifier(SPRINT_SPEED_MODIFIER)) {
+                    attributeInstance.removeModifier(SPRINT_SPEED_MODIFIER);
+                }
+            }
+        super.tick(stack, slot, entity);
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
+        var modifiers = super.getModifiers(stack, slot, entity, uuid);
+        SlotAttributes.addSlotModifier(modifiers, "chest/back", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
+        TEST_UUID = uuid;
+        return modifiers;
     }
 }
