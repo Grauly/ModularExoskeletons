f138e03c06d929797340c82d1d12c9530753818c
Nils Dellemann <nils.dellemann@web.de>
2022-10-29 17:43:33 +0200

    feat: added datagen for models and blockstates

---
+++ src/main/java/grauly/datagen/ModExModelProvider.java
@@ -0,0 +1,42 @@
+package grauly.datagen;
+
+import com.mojang.datafixers.TypeRewriteRule;
+import grauly.block.AllBlocks;
+import grauly.item.AllItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import net.minecraft.item.Item;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+class ModExModelProvider extends FabricModelProvider {
+
+    public ModExModelProvider(FabricDataGenerator dataGenerator) {
+        super(dataGenerator);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleState(AllBlocks.MODIFICATION_TABLE);
+        blockStateModelGenerator.registerSimpleState(AllBlocks.EXO_MODIFICATION_TABLE);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        var itemFields = new ArrayList<Field>(List.of(AllItems.class.getFields()));
+        itemFields.forEach(f -> {
+            try {
+                var item = f.get(null);
+                itemModelGenerator.register((Item) item, Models.GENERATED);
+                System.out.printf("Registered: %s\r\n",f.getName());
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
---
+++ src/main/java/grauly/datagen/ModularExosDatagen.java
@@ -0,0 +1,11 @@
+package grauly.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModularExosDatagen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.addProvider(ModExModelProvider::new);
+    }
+}
